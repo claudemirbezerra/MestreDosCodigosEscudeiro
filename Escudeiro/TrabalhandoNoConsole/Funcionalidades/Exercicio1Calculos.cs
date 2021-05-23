137cf53ab9c78b4897ead73899a044e89aa30e0e
Claudemir Bezerra <claudemir.bezerra@db1.com.br>
2021-05-23 15:13:25 -0300

    Exercicio 1 resolvido.

---
+++ Escudeiro/TrabalhandoNoConsole/Funcionalidades/Exercicio1Calculos.cs
@@ -0,0 +1,77 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TrabalhandoNoConsole.Funcionalidades
+{
+    public static class Exercicio1Calculos
+    {
+        public static Boolean SolicitarPreenchementoDeValoresParaOUsuario()
+        {
+            var primeiroValor = ReceberValorParaCalculo("\nInsira o primeiro valor para os cálculos(para decimais utiliza a vírgula): ");
+            var segundoValor = ReceberValorParaCalculo("\nInsira o segundo valor para os cálculos(para decimais utiliza a vírgula): ");
+
+            RealizarCalculos(primeiroValor, segundoValor);
+
+            Console.WriteLine("Deseja executar algum outro exercício? (s/n)");
+            var continuarPrograma = Console.ReadLine();
+            var continuarProgramaBoolean = continuarPrograma == "s";
+
+            return continuarProgramaBoolean;
+        }
+
+        private static void RealizarCalculos(decimal primeiroValor, decimal segundoValor)
+        {
+            var valorSomado = primeiroValor + segundoValor;
+            var valorSubtraido = primeiroValor - segundoValor;
+            var valorDividido = primeiroValor / segundoValor;
+            var valorMultiplicado = primeiroValor * segundoValor;
+
+            var resultado = RetornarResultadoMensagem(valorSomado, "somados");
+            resultado += RetornarResultadoMensagem(valorSubtraido, "subtraídos");
+            resultado += RetornarResultadoMensagem(valorDividido, "divididos");
+            resultado += RetornarResultadoMensagem(valorMultiplicado, "multiplicados");
+
+            resultado += "\n" + primeiroValor + " - " + RetornarSeValorImparOuPar(primeiroValor);
+            resultado += "\n" + segundoValor + " - " + RetornarSeValorImparOuPar(segundoValor);
+
+            Console.WriteLine(resultado);
+        }
+
+        private static string RetornarSeValorImparOuPar(decimal valor)
+        {
+            var resultado = "Par";
+
+            if ((valor % 2) != 0)
+            {
+                resultado = "Ímpar";
+            }
+
+            return resultado;
+        }
+
+        private static string RetornarResultadoMensagem(decimal valor, string complementoMensagem)
+        {
+            var mensagem = "\nResultado dos valores " + complementoMensagem + " é igual a " + valor;
+            return mensagem;
+        }
+
+        private static decimal ReceberValorParaCalculo(string mensagem)
+        {
+            Console.WriteLine(mensagem);
+            var valorString = Console.ReadLine();
+            decimal valorDecimal;
+
+            if (decimal.TryParse(valorString, out valorDecimal))
+            {
+                return valorDecimal;
+            }
+
+            Console.WriteLine("\nNúmero inválido, escreva em qualquer formato decimal válido.");
+            valorDecimal = ReceberValorParaCalculo(mensagem);
+            return valorDecimal;
+        }
+    }
+}
---
+++ Escudeiro/TrabalhandoNoConsole/Program.cs
@@ -0,0 +1,84 @@
+﻿using System;
+using TrabalhandoNoConsole.Funcionalidades;
+
+namespace TrabalhandoNoConsole
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            SelecionarExercicio();
+        }
+
+        private static void SelecionarExercicio(bool mostrarOpcoes = true)
+        {
+            if (mostrarOpcoes)
+            {
+                var textoOpcoes = "1 - Exercício 1 - Cálculos \n";
+                textoOpcoes += "2 - Exercício 2 - Salários \n";
+                textoOpcoes += "3 - Exercício 3 - Multiplos de 3 \n";
+                textoOpcoes += "4 - Exercício 4 - Notas \n";
+                textoOpcoes += "5 - Exercício 5 - Formula Bhakara \n";
+                textoOpcoes += "6 - Exercício 6 - Diferença Ref e Out \n";
+                textoOpcoes += "7 - Exercício 7 - Soma pares \n";
+                textoOpcoes += "8 - Exercício 8 - Ordenação \n";
+                textoOpcoes += "9 - Exercício 9 - LINQ \n";
+                textoOpcoes += "Entre com uma da opções acima(apenas o número): ";
+
+                Console.WriteLine(textoOpcoes);
+            }
+
+            var valorOpcao = Console.ReadLine();
+            var continuarPrograma = false;
+
+            switch (valorOpcao)
+            {
+                case "1":
+                    continuarPrograma = Exercicio1Calculos.SolicitarPreenchementoDeValoresParaOUsuario();
+                    break;
+                default:
+                    Console.WriteLine("\nOpção inválida, entre com um número válido: ");
+                    SelecionarExercicio(false);
+                    break;
+            }
+
+            if (continuarPrograma)
+            {
+                Console.WriteLine("\n\n");
+                SelecionarExercicio();
+            }
+        }
+
+        private static void SelecionarOpcoesDoExercicio2()
+        {
+        }
+
+        private static void SelecionarOpcoesDoExercicio3()
+        {
+        }
+
+        private static void SelecionarOpcoesDoExercicio4()
+        {
+        }
+
+        private static void SelecionarOpcoesDoExercicio5()
+        {
+        }
+
+        private static void SelecionarOpcoesDoExercicio6()
+        {
+        }
+
+        private static void SelecionarOpcoesDoExercicio7()
+        {
+        }
+
+        private static void SelecionarOpcoesDoExercicio8()
+        {
+        }
+
+        private static void SelecionarOpcoesDoExercicio9()
+        {
+        }
+    }
+}
